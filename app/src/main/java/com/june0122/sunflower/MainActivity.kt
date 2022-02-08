333901ed62be1e4dac3b726bc16eef71235bf444
june0122 <june0122@naver.com>
2022-02-08 21:56:42 +0900

    initial commit

---
+++ app/src/main/java/com/june0122/sunflower/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.june0122.sunflower
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.june0122.sunflower.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/june0122/sunflower/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.june0122.sunflower.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
