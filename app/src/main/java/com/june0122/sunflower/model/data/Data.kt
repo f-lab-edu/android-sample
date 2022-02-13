9a11ecbd95149a5af37cd5178e3740b61c97ef03
june0122 <june0122@naver.com>
2022-02-14 03:25:21 +0900

    refactor: Plant 객체를 Bundle 객체에 저장하기 위해 Serializable을 구현

--- app/src/main/java/com/june0122/sunflower/model/data/Data.kt
+++ app/src/main/java/com/june0122/sunflower/model/data/Data.kt
@@ -1,7 +1,9 @@
 package com.june0122.sunflower.model.data
 
+import java.io.Serializable
+
 data class Plant(
     val imageUrl: String,
     val name: String,
     val description: String,
-    )
+    ): Serializable
