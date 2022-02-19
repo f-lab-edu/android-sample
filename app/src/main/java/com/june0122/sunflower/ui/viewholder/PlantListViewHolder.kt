d082704bec2831846ed657739bfa32abe314dc67
june0122 <june0122@naver.com>
2022-02-19 21:01:55 +0900

    feat: 이미지 로딩 시에 보일 placeholder 추가

--- app/src/main/java/com/june0122/sunflower/ui/viewholder/PlantListViewHolder.kt
+++ app/src/main/java/com/june0122/sunflower/ui/viewholder/PlantListViewHolder.kt
@@ -3,11 +3,13 @@ package com.june0122.sunflower.ui.viewholder
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import coil.size.Scale
+import com.june0122.sunflower.R
 import com.june0122.sunflower.databinding.ItemPlantListBinding
 import com.june0122.sunflower.model.data.Plant
 import com.june0122.sunflower.utils.PlantSelectedListener
 
-class PlantListViewHolder(binding: ItemPlantListBinding, listener: PlantSelectedListener) : RecyclerView.ViewHolder(binding.root) {
+class PlantListViewHolder(binding: ItemPlantListBinding, listener: PlantSelectedListener) :
+    RecyclerView.ViewHolder(binding.root) {
     private val plantNameTextView = binding.tvPlantName
     private val availablePlantImageView = binding.imgAvailablePlant
 
@@ -17,6 +19,7 @@ class PlantListViewHolder(binding: ItemPlantListBinding, listener: PlantSelected
 
     fun bind(plant: Plant) {
         availablePlantImageView.load(plant.imageUrl) {
+            placeholder(R.drawable.ic_placeholder)
             scale(Scale.FIT)
             crossfade(true)
             crossfade(300)
