65b23dbba6d4f8b7f2be2b7dae6d5d89e7a8a705
june0122 <june0122@naver.com>
2022-02-20 01:12:20 +0900

    feat: 아이템 길게 클릭 시 Dialog 띄우기 및 아이템 추가 임시 구현

--- app/src/main/java/com/june0122/sunflower/ui/fragment/PlantListFragment.kt
+++ app/src/main/java/com/june0122/sunflower/ui/fragment/PlantListFragment.kt
@@ -9,8 +9,12 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.june0122.sunflower.R
 import com.june0122.sunflower.databinding.FragmentPlantListBinding
+import com.june0122.sunflower.model.data.Plant
 import com.june0122.sunflower.ui.adapter.PlantListAdapter
+import com.june0122.sunflower.utils.PlantClickListener
 import com.june0122.sunflower.utils.decoration.PlantListItemDecoration
+
+private const val DIALOG_PLANT = "DialogPlant"
 
 class PlantListFragment : Fragment() {
     private var _binding: FragmentPlantListBinding? = null
@@ -25,16 +29,24 @@ class PlantListFragment : Fragment() {
         plantRecyclerView = binding.rvPlantList
         plantRecyclerView.layoutManager = GridLayoutManager(context, 2)
         plantRecyclerView.addItemDecoration(PlantListItemDecoration(2, 60, true))
-        plantListAdapter = PlantListAdapter { position ->
-            val plantData = plantListAdapter.items[position]
-            val plantDetailFragment = PlantDetailFragment.newInstance(plantData)
+        plantListAdapter = PlantListAdapter(object : PlantClickListener {
+            override fun onPlantClick(position: Int) {
+                val plantData = plantListAdapter.items[position]
+                val plantDetailFragment = PlantDetailFragment.newInstance(plantData)
+                requireActivity().supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.container, plantDetailFragment)
+                    .addToBackStack(null)
+                    .commit()
+            }
 
-            requireActivity().supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.container, plantDetailFragment)
-                .addToBackStack(null)
-                .commit()
-        }
+            override fun onPlantLongClick(position: Int) {
+                val plantData = plantListAdapter.items[position]
+                PlantDialogFragment.newInstance(plantData).apply {
+                    show(this@PlantListFragment.parentFragmentManager, DIALOG_PLANT)
+                }
+            }
+        })
         plantRecyclerView.adapter = plantListAdapter
 
         return view
@@ -42,6 +54,19 @@ class PlantListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.fabAddPlant.setOnClickListener {
+            val items = plantListAdapter.items
+            // 데이터를 직접 입력해서 아이템을 추가하는 식으로 변경 예정
+            items.add(
+                Plant(
+                    imageUrl = "https://www.thespruce.com/thmb/-W1mZNWR5tVwvp4reWuEoe0ZEyc=/941x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-902133540-7e603f4ab7aa4aadb097c61d2627da24.jpg",
+                    name = "Quinoa",
+                    description = "Quinoa (Chenopodium quinoa) is a flowering plant in the Amaranth family that is grown as a crop primarily for its edible seeds. It has been grown for human consumption for thousands of years, originating from mountainous regions of South America. Quinoa cultivation has now grown to over 70 countries around the world. This ancient superfood is packed with vitamins and minerals and has a nice mild taste. The seeds are often cooked like rice, or ground into a flour that can be used as a gluten-free alternative in cooking and baking."
+                )
+            )
+            plantListAdapter.notifyItemInserted(items.size)
+        }
     }
 
     override fun onDestroyView() {
