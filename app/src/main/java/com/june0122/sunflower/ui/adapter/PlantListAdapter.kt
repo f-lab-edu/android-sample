49e95b43eeb82c12d33510f1d30c2cc1dd0b0af9
june0122 <june0122@naver.com>
2022-02-22 03:58:14 +0900

    feat: 페이징 시 로딩 표시를 위한 프로그래스바를 담을 뷰 타입 추가
    아이템과 프로그래스바를 위한 뷰타입이 2개 필요한데, 이를 위해선 ViewHolder를 각각의 뷰타입에 맞게
    생성해야 한다.

--- app/src/main/java/com/june0122/sunflower/ui/adapter/PlantListAdapter.kt
+++ app/src/main/java/com/june0122/sunflower/ui/adapter/PlantListAdapter.kt
@@ -4,11 +4,17 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.june0122.sunflower.databinding.ItemPlantListBinding
+import com.june0122.sunflower.databinding.ItemProgressBinding
 import com.june0122.sunflower.model.data.Plant
 import com.june0122.sunflower.ui.viewholder.PlantListViewHolder
+import com.june0122.sunflower.ui.viewholder.ProgressHolder
 import com.june0122.sunflower.utils.PlantClickListener
 
-class PlantListAdapter(private val listener: PlantClickListener) : RecyclerView.Adapter<PlantListViewHolder>() {
+const val VIEW_TYPE_ITEM = 0
+const val VIEW_TYPE_LOADING = 1
+const val STATUS_LOADING = "STATUS_LOADING"
+
+class PlantListAdapter(private val listener: PlantClickListener) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     /**
      * 1. item은 언제든 add, remove, clear 할 수 있어야 한다.
      * 2. item 리스트 변수는 private으로 선언
@@ -16,18 +22,39 @@ class PlantListAdapter(private val listener: PlantClickListener) : RecyclerView.
      */
     val items = mutableListOf<Plant>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlantListViewHolder {
-        val binding =
-            ItemPlantListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    override fun getItemViewType(position: Int): Int {
+        return when(items[position].description) {
+            STATUS_LOADING -> VIEW_TYPE_LOADING
+            else -> VIEW_TYPE_ITEM
+        }
+    }
 
-        return PlantListViewHolder(binding, listener)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when(viewType) {
+            VIEW_TYPE_ITEM -> {
+                val binding =
+                    ItemPlantListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                PlantListViewHolder(binding, listener)
+            }
+            else -> {
+                val binding =
+                    ItemProgressBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                ProgressHolder(binding)
+            }
+        }
     }
 
     override fun getItemCount(): Int = items.count()
 
-    override fun onBindViewHolder(holder: PlantListViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val plant = items[holder.absoluteAdapterPosition]
 
-        holder.bind(plant)
+        if (holder is PlantListViewHolder) holder.bind(plant)
+    }
+
+    fun deleteProgress() {
+        if (items.last().description == STATUS_LOADING) {
+            items.removeAt(items.lastIndex)
+        }
     }
 }
---
+++ app/src/main/java/com/june0122/sunflower/ui/viewholder/ProgressHolder.kt
@@ -0,0 +1,6 @@
+package com.june0122.sunflower.ui.viewholder
+
+import androidx.recyclerview.widget.RecyclerView
+import com.june0122.sunflower.databinding.ItemProgressBinding
+
+class ProgressHolder(binding: ItemProgressBinding) : RecyclerView.ViewHolder(binding.root)
