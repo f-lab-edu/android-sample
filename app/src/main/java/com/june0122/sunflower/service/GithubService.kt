e9ddde702e6aec356c74cfb44b4e38f6d9eacbaa
june0122 <june0122@naver.com>
2022-03-04 03:22:52 +0900

    refactor: Service와 Retrofit 인스턴스 생성 코드 분리

---
+++ app/src/main/java/com/june0122/sunflower/service/GithubService.kt
@@ -0,0 +1,20 @@
+package com.june0122.sunflower.service
+
+import com.june0122.sunflower.model.data.UserDetail
+import com.june0122.sunflower.model.data.Users
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface GithubService {
+    @GET("search/users")
+    fun getUserList(
+        @Query("q") query: String,
+        @Query("per_page") perPage: Int,
+        @Query("page") page: Int,
+    ): Call<Users>
+
+    @GET("users/")
+    fun getUserDetail(@Path("username") userName: String): Call<UserDetail>
+}
