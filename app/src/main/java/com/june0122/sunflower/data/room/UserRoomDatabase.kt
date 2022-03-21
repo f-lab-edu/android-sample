faef5685b110882ea6fc9be71328d22a4e5eae80
june0122 <june0122@naver.com>
2022-03-22 01:08:19 +0900

    refactor: UserDatabaseCallback 분리

--- app/src/main/java/com/june0122/sunflower/data/room/UserRoomDatabase.kt
+++ app/src/main/java/com/june0122/sunflower/data/room/UserRoomDatabase.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.june0122.sunflower.data.entity.User
 import kotlinx.coroutines.CoroutineScope
 
@@ -12,28 +11,6 @@ import kotlinx.coroutines.CoroutineScope
 abstract class UserRoomDatabase : RoomDatabase() {
 
     abstract fun userDao(): UserDao
-
-    private class UserDatabaseCallback(private val scope: CoroutineScope) : RoomDatabase.Callback() {
-        override fun onCreate(db: SupportSQLiteDatabase) {
-            super.onCreate(db)
-            // 앱 재실행 시에도 데이터를 유지하려면 아래의 코드 주석 처리
-//            INSTANCE?.let { database ->
-//                scope.launch(Dispatchers.IO) {
-//                    populateDatabase(database.userDao())
-//                }
-//            }
-        }
-
-        suspend fun populateDatabase(userDao: UserDao) {
-            userDao.deleteAll()
-
-            // 디폴트 데이터 추가
-//            var user = User("", "", "")
-//            userDao.insert(user)
-//            user = User("", "", "")
-//            userDao.insert(user)
-        }
-    }
 
     companion object {
         @Volatile
@@ -53,21 +30,4 @@ abstract class UserRoomDatabase : RoomDatabase() {
             }
         }
     }
-
-//    companion object {
-//        private const val databaseName = "user_database"
-//        private var appDatabase: UserRoomDatabase? = null
-//
-//        fun getDatabase(context: Context): UserRoomDatabase? {
-//            if (appDatabase == null) {
-//                appDatabase = Room.databaseBuilder(
-//                    context,
-//                    UserRoomDatabase::class.java,
-//                    databaseName
-//                ).build()
-//            }
-//
-//            return appDatabase
-//        }
-//    }
 }
